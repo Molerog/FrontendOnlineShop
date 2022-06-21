58a509711a04f40ad32bcd55cbaf598393cef99d
Molerog <102958319+Molerog@users.noreply.github.com>
2022-06-21 15:46:51 +0200

    añadido logout y componentes condicionales

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,49 @@
 import React from "react";
 import "./Header.scss";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserState";
 
 const Header = () => {
-  // const { token, logout } = useContext(UserContext);
-  // const logoutUser = () => {
-  //   logout();
-  // };
+  const { token, logout } = useContext(UserContext);
+  const logoutUser = () => {
+    logout();
+  };
   return (
     <header>
       <div className="overlay">
-          <h1>Simply The Best</h1>
-          <h3>Reasons for Choosing US</h3>
-          <div className="LinksContainer">
+        <h1>Simply The Best</h1>
+        <h3>Reasons for Choosing US</h3>
+        <div className="LinksContainer">
+          <span>
             <Link to="/home">Home</Link>
+          </span>
+          {token ? (
+          <>
+          <span>
             <Link to="/products">Products</Link>
+          </span>
+          <span>
             <Link to="/profile">Profile</Link>
-            <div className="ButtonsContainer">
-              <button>
-                <Link to="/">Register</Link>
-              </button>
-              <button>
-                <Link to="/login">Login</Link>
-              </button>
-            </div>
+          </span>
+          <button onClick = {logoutUser}>
+            <Link to= '/register'>Logout</Link>
+          </button>
+          </>
+          ):(
+          <>
+          <div className="ButtonsContainer">
+            <button>
+              <Link to="/">Register</Link>
+            </button>
+            <button>
+              <Link to="/login">Login</Link>
+            </button>
           </div>
+            </>
+            )}
         </div>
+      </div>
     </header>
   );
 };
