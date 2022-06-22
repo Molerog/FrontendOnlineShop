7fe2e1c306304c19c53fb24d7858f27b525e1f5f
Rebeca <rebeca.as1994@gmail.com>
2022-06-22 13:03:42 +0200

    Profile muestra Orders

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,19 +2,31 @@ import { useContext, useEffect } from "react";
 import { UserContext } from "../../context/UserState";
 
 const Profile = () => {
-  const { getUserInfo, user } = useContext(UserContext);
-  useEffect(() => {
-    getUserInfo();
-  }, []);
+    const { getUserInfo, user } = useContext(UserContext);
 
-  if (!user) {
-    return <span>Cargando...</span>;
-  } 
-  
-    return <div>{user.user.name}</div>;
+    useEffect(() => {
+        getUserInfo()
+    }, []);
 
-  
+    if (!user) {
+        return <span>Cargando...</span>
+    } 
+    
+        return (
+            <>
+                <div>
+                    <h3>Datos personales</h3>
+                    <span>{user.user.name}</span>
+                    <br />
+                    <span>{user.user.email}</span>
+                </div>
+                <br />
+                <div>
+                    <h3>Pedidos</h3>
+                </div>
+            </>
+        )
+        
+}
 
-};
-
-export default Profile;
+export default Profile
