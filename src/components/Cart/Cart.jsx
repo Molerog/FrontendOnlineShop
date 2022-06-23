b838b5ec9831341ee68c22ac1ae691cd1dd8c6b0
Molerog <102958319+Molerog@users.noreply.github.com>
2022-06-23 15:31:46 +0200

    añadido modal

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,42 +1,88 @@
 import { useContext, useEffect } from "react";
 import { OrderContext } from "../../context/OrderState";
 import { ProductContext } from "../../context/ProductState";
-
+import { Modal } from "antd";
+import { useState } from "react";
+import {useNavigate} from 'react-router-dom'
 
 const Cart = () => {
-    const { cart,clearCart } = useContext(ProductContext);
-    const { createOrder } = useContext(OrderContext);
-    useEffect(() => {
-        localStorage.setItem("cart", JSON.stringify(cart))
-    }, [cart]);
+  const products = JSON.parse(localStorage.getItem("cart"));
 
-    if (cart.length <= 0) {
-        return <span>No tienes ningún producto en tu carrito</span>
-    };
+  const { cart, clearCart } = useContext(ProductContext);
+  const { createOrder } = useContext(OrderContext);
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState(products.map(e => {
+        return <span>{e.product}</span>
+  }));
+  const navigate = useNavigate()
 
-    const createNewOrder = () => {
-        createOrder(cart);
-        // clearCart();
-    };
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = (useEffect) => {
+    setModalText('Tu compra ha sido realizada con éxito');
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+      clearCart()
+      navigate('/profile')
+    }, 2000);
     
+};
+// useEffect(() => {       
+//   navigate("/profile")
+// } , [])
 
-    const cartItem = cart.map((cartItem, i) => {
-        return (
-            <div className="cart" key={i}>
-                <span>{cartItem.product}:    </span>
-                <span> {cartItem.price.toFixed(2)} €</span>
-            </div>
-        )
-    });
 
+  const handleCancel = () => {
+    console.log("Clicked cancel button");
+    setVisible(false);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  if (cart.length <= 0) {
+    return <span>No tienes ningún producto en tu carrito</span>;
+  }
+
+  const createNewOrder = (showModal) => {
+    showModal();
+    createOrder(cart);
+
+    // clearCart();
+  };
+
+  const cartItem = cart.map((cartItem, i) => {
     return (
-        <div>
-            {cartItem}
-            <p>Cantidad total de productos: {cart.length}</p>
-            <button onClick={() => clearCart()}>Vacía tu carrito</button>
-            <button onClick={() => createNewOrder()}>Crea tu pedido</button>
-        </div>
-    )
-}
+      <div className="cart" key={i}>
+        <span>{cartItem.product}</span>
+        <span> {cartItem.price.toFixed(2)} €</span>
+      </div>
+    );
+  });
 
-export default Cart
+  return (
+    <div>
+      {cartItem}
+      <p>Cantidad total de productos: {cart.length}</p>
+      <button onClick={() => clearCart()}>Vacía tu carrito</button>
+      <button onClick={() => createNewOrder(showModal)}>Crea tu pedido</button>
+      <Modal
+        title="¿Deseas continuar con la compra?"
+        visible={visible}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <p>{modalText}</p>
+      </Modal>
+    </div>
+  );
+};
+
+export default Cart;
